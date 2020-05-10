6d3ddb716b27ffb944ceeea7e5b89c5d512ccf36
Dara <dara.dandashi@gmail.com>
2020-05-10 22:42:35 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
-// Write your code here
+///--------------multiply
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1*num2;
+console.log(multiply);
+
+///--------------rabdom
+
+const random = Math.random();
+console.log(random);
+
+///--------------mod
+// let no = 32;
+// const mod =(no %= 2);
+
+// let no = 32;
+const mod = Number.parseInt('100', 2);
+console.log(mod);
+
+// Number.parseInt('100', 2)
+///--------------max
+
+let set = (1, 2, 3, 20)
+const max = Math.max(set);
+console.log(max);
